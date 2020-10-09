45c4d1ea708be8a963d323ca64c4165fcdbdb69d
99002637 <72497580+99002637@users.noreply.github.com>
2020-10-09 09:33:37 +0530

    Delete number.h

--- inc/number.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CALCULATOR_OPERATIONS_H__
-#define __CALCULATOR_OPERATIONS_H__
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-int add(int number1,int number2);
-int difference(int number1,int number2);
-#endif
